ff70a75757f33f9f0259425f05da2b32ec304955
bavithras <bavi.rane@gmail.com>
2019-03-05 13:05:49 +0530

    first commit

---
+++ src/main/java/com/webservice/consumption/webserviceconsumption/WebserviceconsumptionApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.consumption.webserviceconsumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceconsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceconsumptionApplication.class, args);
+	}
+
+}
